d7c3d270bab24774bb8a39183771234a050b21bc
drako900 <josue_israel@hotmail.com>
2013-07-14 21:02:42 -0500

    Create BankItem.java

---
+++ com/github/drako900/BankItem.java
@@ -0,0 +1,25 @@
+package com.github.drako900;
+
+import org.bukkit.entity.Player;
+
+public class BankItem {
+  private int status;
+	private Player player;
+	
+	BankItem(Player player, String status){
+		this.player = player;
+		this.status = Integer.parseInt(status);
+	}
+	
+	public void setstatustransfer(int status){
+		this.status = status;
+	}
+	
+	public Player getPlayer() {
+		return player;
+	}
+	
+	public int getstatus(){
+		return status;
+	}
+}

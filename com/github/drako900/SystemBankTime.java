88116b40e9e9a777bfa41a3d4f1900f9afdf3423
drako900 <josue_israel@hotmail.com>
2013-09-07 22:16:07 -0500

    Update - version 1.6.5

--- com/github/drako900/SystemBankTime.java
+++ com/github/drako900/SystemBankTime.java
@@ -5,13 +5,15 @@ import java.io.IOException;
 
 import net.milkbowl.vault.economy.Economy;
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class SystemBankTime {
-     //ECONOMY
+	   //ECONOMY
 		public static Economy econ = null;
 	   public boolean setupEconomy(){
 	       RegisteredServiceProvider<Economy> rsp = plugin.getServer().getServicesManager().getRegistration(Economy.class);
@@ -50,14 +52,44 @@ public class SystemBankTime {
 	            if(fileNames.length>0){
 	            	for(int i = 0; i < fileNames.length; i++)
 	            	{
+	            		//The player is online?
+            			OfflinePlayer pTo = plugin.getServer().getPlayer(fileNames[i].replace(".yml", ""));
+            			
+            			if (pTo == null) {
+            			    pTo = plugin.getServer().getOfflinePlayer(fileNames[i].replace(".yml", ""));
+            			    
+            			}
+            			
+            			Player player = pTo.getPlayer();
+	            		
+	            		if(player == null){
+	            			
+	            			
+	            		}else{
+	            		
+	            		if(player.isOnline()){
+	            		
 	                	File datosp = new File(plugin.getDataFolder() + File.separator + "pdata" + File.separator + fileNames[i]);
 	                	FileConfiguration datosp2 = YamlConfiguration.loadConfiguration(datosp);
 	                	
 	                	//GET DATA OF BANK MONEY...
 	                	int current_time = 0;
 	                	current_time = datosp2.getInt("banktime.time-save");
-	                	int future_time = current_time + 1;
+	                	int future_time = current_time + plugin.getConfig().getInt("BankTime.give-money-per-period");
 	                	
+	                	//Max money give reached?
+	                	int max_give_money = plugin.getConfig().getInt("BankTime.max-money-give");
+	                	boolean max_g_m_r = false;
+	                	
+	                	if(max_give_money <= -1){
+	                		
+	                	}else{
+	                		if(current_time>max_give_money){
+	                			max_g_m_r = true;
+		                	}
+	                	}
+	                	
+	                	if(!max_g_m_r){
 	                		datosp2.set("banktime.time-save", future_time);
 	                	
 	                		try {
@@ -66,10 +98,15 @@ public class SystemBankTime {
 	                			// TODO Auto-generated catch block
 	                			e.printStackTrace();
 	                		}
+	                	}else{
+	                		max_g_m_r = false;
+	                	}
 	                	
+	            		}
+	            		}
 	            	}
 	            }
 			}
-		}.runTaskTimerAsynchronously(plugin, 40, 1200);
+		}.runTaskTimerAsynchronously(plugin, 40, (1200 * plugin.getConfig().getInt("BankTime.time-per-period")));
 	}
 }
